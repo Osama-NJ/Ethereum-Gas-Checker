b6659c1e451f3d7341011e1832836c42869348d8
Osama Najjar <76047819+Osama-NJ@users.noreply.github.com>
2022-04-14 02:48:37 +0300

    First commit

---
+++ main/EthereumGas.py
@@ -0,0 +1,24 @@
+import json
+import requests
+import jsonpath
+
+
+def ethereum_gas():
+    """
+    This function will get Ethereum gas fees in all transaction speeds from EthGasStation.info
+    """
+    url = 'https://ethgasstation.info/api/ethgasAPI.json?'  # API url
+    respond = requests.get(url)  # Sending the request to the API server
+    gas = json.loads(respond.text)  # Converting the request data to JSON
+    safe_low = jsonpath.jsonpath(gas, "safeLow")  # Fetching safe low eth gas
+    average = jsonpath.jsonpath(gas, "average")   # Fetching average eth gas
+    fast = jsonpath.jsonpath(gas, "fast")  # Fetching fast eth gas
+    fastest = jsonpath.jsonpath(gas, "fastest")  # Fetching fastest eth gas
+    print("Safe low Ethereum gas fees speed : " + str(int(safe_low[0])/10) + " GWEI")
+    print("Average Ethereum gas fees speed : " + str(int(average[0])/10) + " GWEI")
+    print("Fast Ethereum gas fees speed : " + str(fast[0]/10) + " GWEI")
+    print("Fastest Ethereum gas fees speed : " + str(fastest[0]/10) + " GWEI")
+
+
+if __name__ == "__main__":
+    ethereum_gas()
